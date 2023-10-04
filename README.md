27249aae0f584701073d5a9f2f29b24804e7ac91
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2023-10-04 15:03:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Some utils related to interacting with PHP lsp servers
+
+## Servers:
+- phpactor
+- intelephense
+
+## Actions:
+- go_to
---
+++ lua/php-lsp-utils/init.lua
@@ -0,0 +1,49 @@
+local phpactor = require "php-lsp-utils.phpactor"
+local intelephense = require "php-lsp-utils.intelephense"
+
+local servers = {
+  phpactor = phpactor,
+  intelephense = intelephense,
+}
+
+---@param server_name string
+---@return table|nil, boolean
+local get_client = function(server_name)
+  local clients = vim.lsp.get_active_clients { name = server_name }
+  local client = clients[1] or nil
+  local new_instance = false
+
+  if not client then
+    local server = require("lspconfig")[server_name]
+    local client_id = vim.lsp.start(server.make_config(vim.fn.getcwd()))
+    client = vim.lsp.get_client_by_id(client_id)
+    new_instance = true
+  end
+
+  return client, new_instance
+end
+
+---@param full_class string
+---@param method string
+local go_to = function(full_class, method)
+  local server_name = config.options.lsp_server
+
+  local server = servers[server_name]
+  if server == nil then
+    notify("Route open", { msg = "No server name " .. server_name, level = "WARN" })
+    return
+  end
+
+  local client, is_new_instance = get_client(server_name)
+
+  if not client then
+    notify("Route open", { msg = "Can't get lsp client", level = "WARN" })
+    return
+  end
+
+  return server.go_to(client, is_new_instance, full_class, method)
+end
+
+return {
+  go_to = go_to,
+}
