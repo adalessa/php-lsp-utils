02ce97a78fe886b7fc27242086034bf3be562f6a
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2025-04-14 08:34:36 +0200

    support non lsp config configurations

--- lua/php-lsp-utils/init.lua
+++ lua/php-lsp-utils/init.lua
@@ -1,50 +1,58 @@
-local phpactor = require "php-lsp-utils.phpactor"
-local intelephense = require "php-lsp-utils.intelephense"
+local phpactor = require("php-lsp-utils.phpactor")
+local intelephense = require("php-lsp-utils.intelephense")
 
 local servers = {
-    phpactor = phpactor,
-    intelephense = intelephense,
+	phpactor = phpactor,
+	intelephense = intelephense,
 }
 
 ---@param server_name string
 ---@return table|nil, boolean
 local get_client = function(server_name)
-    local clients = vim.lsp.get_active_clients { name = server_name }
-    local client = clients[1] or nil
-    local new_instance = false
+	local clients = vim.lsp.get_clients({ name = server_name })
+	local client = clients[1] or nil
+	local new_instance = false
 
-    if not client then
-        local server = require("lspconfig")[server_name]
-        local client_id = vim.lsp.start(server.make_config(vim.fn.getcwd()))
-        if client_id == nil then
-            return nil, false
-        end
-        client = vim.lsp.get_client_by_id(client_id)
-        new_instance = true
-    end
+	if not client then
+		local ok, lsp_config = pcall(require, "lspconfig")
+		local config = {}
+		if not ok then
+			config = vim.lsp.config[server_name]
+		else
+			local server = lsp_config[server_name]
+			config = server.make_config(vim.loop.cwd())
+		end
 
-    return client, new_instance
+		local client_id = vim.lsp.start(config)
+		if not client_id then
+			error("Cold not start lsp client")
+		end
+		client = vim.lsp.get_client_by_id(client_id)
+		new_instance = true
+	end
+
+	return client, new_instance
 end
 
 ---@param full_class string
 ---@param method string
 local go_to = function(server_name, full_class, method)
-    local server = servers[server_name]
-    if server == nil then
-        vim.notify("Server not valid", vim.log.levels.WARN, {})
-        return
-    end
+	local server = servers[server_name]
+	if server == nil then
+		vim.notify("Server not valid", vim.log.levels.WARN, {})
+		return
+	end
 
-    local client, is_new_instance = get_client(server_name)
+	local client, is_new_instance = get_client(server_name)
 
-    if not client then
-        vim.notify("Can't get lsp client", vim.log.levels.WARN, {})
-        return
-    end
+	if not client then
+		vim.notify("Can't get lsp client", vim.log.levels.WARN, {})
+		return
+	end
 
-    return server.go_to(client, is_new_instance, full_class, method)
+	return server.go_to(client, is_new_instance, full_class, method)
 end
 
 return {
-    go_to = go_to,
+	go_to = go_to,
 }
